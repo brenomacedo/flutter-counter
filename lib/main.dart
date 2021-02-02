7fb3d68416ecbe500533bbe102b1829e987510f2
brenosekiro <b88660501@gmail.com>
2021-02-02 17:55:51 -0300

    final

--- lib/main.dart
+++ lib/main.dart
@@ -3,29 +3,92 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador de Pessoas",
-    home: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
+    home: Home(5) 
+  ));
+}
+
+class Home extends StatefulWidget {
+
+  final int amount;
+  Home(this.amount);
+
+  @override
+  _HomeState createState() => _HomeState(amount);
+}
+
+class _HomeState extends State<Home> {
+
+  final int amount;
+  _HomeState(this.amount);
+
+  int _people = 0;
+  String _status = 'Pode entrar!';
+
+  void _changePeople(int delta) {
+    setState(() {
+      int newpeople = _people + delta;
+
+      if(newpeople > 10 || newpeople < 0) {
+        return;
+      }
+
+      _people += delta;
+
+      if(_people == 10) {
+        _status = 'Lotado!';
+      }else{
+        _status = 'Pode entrar!';
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
       children: <Widget>[
-        Text("Pessoas: 0", style: TextStyle(
-          color: Colors.white, fontWeight: FontWeight.bold
-        ),),
-        Row(
+        FractionallySizedBox(
+          widthFactor: 1,
+          heightFactor: 1,
+          child: Image.asset(
+            'images/bg.jpg',
+            fit: BoxFit.cover,
+          )
+        ),
+        Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            FlatButton(
-              child: Text("+1", style: TextStyle(color: Colors.white, fontSize: 30.0)),
-              onPressed: () {},
+            Text("Pessoas: $_people", style: TextStyle(
+              color: Colors.white, fontWeight: FontWeight.bold
+            ),),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text("+1", style: TextStyle(color: Colors.white, fontSize: 30.0)),
+                    onPressed: () {
+                      _changePeople(amount);
+                    },
+                  )
+                ),
+                Padding(
+                  padding: EdgeInsets.all(10.0),
+                  child: FlatButton(
+                    child: Text("-1", style: TextStyle(color: Colors.white, fontSize: 30.0)),
+                    onPressed: () {
+                      _changePeople(-1*amount);
+                    },
+                  )
+                )
+              ],
             ),
-            FlatButton(
-              child: Text("-1", style: TextStyle(color: Colors.white, fontSize: 30.0)),
-              onPressed: () {},
-            )
-          ],
-        ),
-        Text("Pode entrar!",
-        style: TextStyle(color: Colors.white, fontSize: 30.0, fontStyle: FontStyle.italic))
+            Text("$_status",
+            style: TextStyle(color: Colors.white, fontSize: 30.0, fontStyle: FontStyle.italic))
 
+          ],
+        )
       ],
-    )
-  ));
+    );
+  }
 }
