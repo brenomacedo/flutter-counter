4284315fb0a15d22d38374ab547639ef097a86d0
brenosekiro <b88660501@gmail.com>
2021-02-02 16:41:09 -0300

    widgets

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,29 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     title: "Contador de Pessoas",
-    home: Container(color: Colors.white)
+    home: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text("Pessoas: 0", style: TextStyle(
+          color: Colors.white, fontWeight: FontWeight.bold
+        ),),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            FlatButton(
+              child: Text("+1", style: TextStyle(color: Colors.white, fontSize: 30.0)),
+              onPressed: () {},
+            ),
+            FlatButton(
+              child: Text("-1", style: TextStyle(color: Colors.white, fontSize: 30.0)),
+              onPressed: () {},
+            )
+          ],
+        ),
+        Text("Pode entrar!",
+        style: TextStyle(color: Colors.white, fontSize: 30.0, fontStyle: FontStyle.italic))
+
+      ],
+    )
   ));
 }
